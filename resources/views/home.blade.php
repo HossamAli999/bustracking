988e62174c17a8ce62d9081a84d2f3b5f18da6df
Hossam Ali <64331041+HossamAli999@users.noreply.github.com>
2022-07-03 04:32:41 +0200

    123

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,12 +11,10 @@
                 <div class="page-title-box">
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                            <li class="breadcrumb-item active">CRM</li>
+                            <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                     </div>
-                    <h4 class="page-title">CRM</h4>
+                    <h4 class="page-title">Dashboard</h4>
                 </div>
             </div>
         </div>     
@@ -28,8 +26,8 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-6">
-                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Campaign Sent">Campaign Sent</h5>
-                                <h3 class="my-2 py-1">9,184</h3>
+                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Campaign Sent">Drivers</h5>
+                                <h3 class="my-2 py-1">{{  $countdriver }}</h3>
                                 <p class="mb-0 text-muted">
                                     <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 3.27%</span>
                                 </p>
@@ -49,8 +47,8 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-6">
-                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">New Leads</h5>
-                                <h3 class="my-2 py-1">3,254</h3>
+                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="New Leads">Buses</h5>
+                                <h3 class="my-2 py-1">{{  $countvehicle }}</h3>
                                 <p class="mb-0 text-muted">
                                     <span class="text-danger me-2"><i class="mdi mdi-arrow-down-bold"></i> 5.38%</span>
                                 </p>
@@ -70,8 +68,8 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-6">
-                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Deals">Deals</h5>
-                                <h3 class="my-2 py-1">861</h3>
+                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Deals">Parents</h5>
+                                <h3 class="my-2 py-1">{{  $countfather }}</h3>
                                 <p class="mb-0 text-muted">
                                     <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 4.87%</span>
                                 </p>
@@ -91,8 +89,8 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-6">
-                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Booked Revenue">Booked Revenue</h5>
-                                <h3 class="my-2 py-1">$253k</h3>
+                                <h5 class="text-muted fw-normal mt-0 text-truncate" title="Booked Revenue">Children</h5>
+                                <h3 class="my-2 py-1">{{ $countchild }}</h3>
                                 <p class="mb-0 text-muted">
                                     <span class="text-success me-2"><i class="mdi mdi-arrow-up-bold"></i> 11.7%</span>
                                 </p>
